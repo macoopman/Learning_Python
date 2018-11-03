068e79147c069562aa075c3be2ae003dcba86b45
Mike Coopman <macoopman@gmail.com>
2018-11-03 15:38:03 -0700

    cleanup

--- Exercises/Assembly_Simulator/assemblySimulator.py
+++ Exercises/Assembly_Simulator/assemblySimulator.py
@@ -45,21 +45,3 @@ class Assembly(object):
 
 
 
-program = Assembly()
-program.WRITE(1)
-program.STORE(1)
-
-program.WRITE(2)
-program.STORE(2)
-
-program.WRITE(88)
-program.STORE(3)
-
-program.LOAD(1)
-program.ADD(0)
-program.STORE(0)
-
-program.WRITE(100)
-program.STORE(4)
-
-program.__repr__()
